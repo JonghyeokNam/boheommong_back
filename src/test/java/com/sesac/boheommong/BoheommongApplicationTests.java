3e1803712d29d197ace1153815d07d3e5ac760fb
JonghyeokNam <njh9750@naver.com>
2025-02-12 11:06:29 +0900

    Initial commit

---
+++ src/test/java/com/sesac/boheommong/BoheommongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.boheommong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoheommongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
